0ef9152a3be6a1542b5a715b7f286fcc88c12523
Andrej Oravkin <aoravkin@gmail.com>
2020-05-04 16:13:17 +0100

    first commit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const blog = () => {
+  return (
+    <Layout>
+      <div>hello from blog page</div>
+    </Layout>
+  )
+}
+
+export default blog
