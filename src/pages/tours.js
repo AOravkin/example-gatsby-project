fae49a16007ac74e759848968ba772fb5218427f
Andrej Oravkin <aoravkin@gmail.com>
2020-05-14 14:33:08 +0100

    Tous and TourList components added

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -2,11 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
+import Tours from "../components/Tours/Tours"
 
 const tours = ({ data }) => {
   return (
     <Layout>
       <StyledHero img={data.toursBcg.childImageSharp.fluid} />
+      <Tours />
     </Layout>
   )
 }
